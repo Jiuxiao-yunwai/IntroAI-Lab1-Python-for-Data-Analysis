15d9e0b6e05deefdc77c6dc8088b6d3c910d0770
柳潇潇 <liuxiao20050617@163.com>
2024-09-22 15:33:47 +0800

    初步分析数据

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 path="titanic.csv"
-
+endl="\n-----------------------------------------------------------------------------------\n"
 data=pd.read_csv(path)
 
-print(data.info())
+def output(title,df):
+    print(title,':')
+    print(df,end=endl)
 
 
+data.info()
+output("Head",data.head())
+output("describe",data.describe())
